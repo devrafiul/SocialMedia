7d53da55c5cac2e5113f5ee9168189ce646c57bb
Rafi <rafiulislam432060@gmail.com>
2024-01-06 12:11:05 +0600

    this is the reducer part of Redux

---
+++ src/Redux/Auth/auth.reducer.js
@@ -0,0 +1,36 @@
+import { LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS, REGISTER_FAILURE, REGISTER_REQUEST, REGISTER_SUCCESS } from "./auth.actionType";
+
+const intitalState={
+    jwt:null,
+    error:null,
+    loading:false
+
+}
+
+
+export const authReducer=(state=intitalState,action)=>{
+
+
+    switch (action.type) {
+        case LOGIN_REQUEST:
+        case REGISTER_REQUEST:
+
+            return { ...state, loading:true,error:null }
+
+
+        case LOGIN_SUCCESS:
+        case REGISTER_SUCCESS:
+
+            return {...state, jwt:action.payload, loading:false, erro:null}
+        
+        
+        case LOGIN_FAILURE:
+        case REGISTER_FAILURE:
+            return {...state,loading:false,error:action.payload}
+        
+    
+        default:
+            return state;
+    }
+
+}
